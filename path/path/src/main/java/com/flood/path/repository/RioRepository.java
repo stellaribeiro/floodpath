ba1d5b5089824ee386458ff936d69cee6462110d
Ryan Silva <ryan.h.silva10@aluno.senai.br>
2026-09-09 09:15:47 -0300

    Delete path/path directory

--- path/path/src/main/java/com/flood/path/repository/RioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flood.path.repository;
-
-import com.flood.path.entity.Rio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RioRepository extends JpaRepository<Rio, Long> {
-}
